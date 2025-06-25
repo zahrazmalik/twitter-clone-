8c38c3dffa87496547fe05d0975f8809a9f9ba14
zahrazmalik <zahrazm.malik@gmail.com>
2025-06-25 11:16:53 +0500

    Sign_in, sign_up and Forget Password

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_24_143157) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_25_054316) do
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,7 +26,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_24_143157) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
